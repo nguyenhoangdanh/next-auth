6bf31a29051dcce083d68cb81df8e321c923a071
nguyenhoangdanh.io <hoangdanh54317@gmail.com>
2024-06-07 13:21:47 +0700

    test

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DashboardPage = () => {
+  return (
+    <div>DashBoardPage</div>
+  )
+}
+
+export default DashboardPage
---
+++ app/dashboard/settings/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Anything = () => {
+  return (
+    <div>SettingsPage</div>
+  )
+}
+
+export default Anything
